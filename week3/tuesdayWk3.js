f456e05dfbc75e8c2d2b6b63032679092f310c86
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-03-28 16:30:11 -0500

    added notes on exercises

--- week3/tuesdayWk3.js
+++ week3/tuesdayWk3.js
@@ -1,1 +1,44 @@
-//test
+//! Declare Keys/Values: given the following object declaration, declare values.
+
+const obj = {};
+
+obj.firstKey = "firstValue"
+obj["numeric"] = 2
+obj.boolean = false
+obj["object"] = {};
+
+console.log(obj['firstKey']);       // firstValue
+console.log(obj['numeric']);        // 2
+console.log(obj['boolean']);        // false
+console.log(obj['object']);         // {}
+
+//! Array of Objects: Write a function printNames that prints the name of every object in the array.
+
+const users = [
+    {
+        name: "Gerald",
+        age: 23,
+        height: 68
+    },
+    {
+        name: "Winnie",
+        age: 35,
+        height: 62
+    },
+    {
+        name: "Peter",
+        age: 61,
+        height: 72
+    }
+]
+
+
+function printNames(array) {
+    let names = "";
+
+    for (i = 0; i > array.length; i++) {
+        console.log(array[i].name);
+    }
+}
+
+printNames(users)
