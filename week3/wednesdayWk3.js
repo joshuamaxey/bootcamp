3c202bb70db5d6a8bfb74e2836ba0e9aa4a79e33
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-03-30 19:26:59 -0500

    Added notes on primitives vs objects

---
+++ week3/wednesdayWk3.js
@@ -0,0 +1,24 @@
+//! References vs. Primitives
+
+//?PRIMITIVES VS OBJECTS
+
+//& FIVE PRIMITIVE TYPES (IMMUTABLE. Values cannot be changed, only reassigned)
+
+//&"Immutable = cannot be changed in memory" - removes
+
+//& When you re-assign the value of a string, you are just reassigning it to a new value, not actually changing its initial value.
+
+/*
+1. Boolean (true and false)
+2. Null - represents the INTENTIONAL absence of value.
+3. Undefined - default return value for many things in JavaScript.
+4. Number - numerical values
+5. String - text data enclosed in quotes
+*/
+
+//& ONE REFERENCE TYPE (MUTABLE. Values can be changed.)
+
+/*
+Object (data stored in the form of key-value pairs, unordered/without indices)
+Array (also an object)
+*/
