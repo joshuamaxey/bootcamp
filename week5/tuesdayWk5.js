5b01a2eea9c5cfcdb2ffa7e88a63c4998908aa6e
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-08 20:14:13 -0500

    Added week5

---
+++ week5/tuesdayWk5.js
@@ -0,0 +1,16 @@
+//! The Call Stack
+
+// The CALL STACK is a structure JavaScript uses to keep track of the evaluation of function calls. It uses the STACK data structure. IN computer science, a STACK is a general pattern of organizing a collection of items. FOr our current use of a stack, the items being organized are our function calls (which occur during the execution of our program).
+
+// Imagine a STACK as a vertical pile that obeys the following pattern:
+
+//* New items are PUSHED to the top of the pile
+//* The only item that can be removed is the TOP of the pile. We call this POPPING the top item from the stack.
+
+
+
+//^ Learning Objectives:
+
+//^ 1. identify the two operations that characterize a STACK data structure
+
+//^ 2. sketch how the CALL STACK is manipulated during the runtime of a simple program like the one provided in this lesson.
