05f25fb1875e2d95204a219b5b165d6d90be708a
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-10 12:12:07 -0500

    added more notes on wednesdayWk5

--- week5/wednesdayWk5.js
+++ week5/wednesdayWk5.js
@@ -6,4 +6,14 @@
 //^ 2 The function has an END STATE (base case)
 //^ 3. The function MOVES CLOSER to the base case with each call.
 
-//^ Also recall: There is a BASE CASE, a RECURSIVE CASE, and a RECURSIVE STEP.
+// Also recall: There is a BASE CASE, a RECURSIVE CASE, and a RECURSIVE STEP.
+
+//^ The BASE CASE is the code that stops the function from runnin. Usually returns a value.
+//^ The RECURSIVE CASE is the code that calls the function within itself. Usually loops until the function achieves the base case.
+//^ The RECURSIVE STEP is the code that actually advances the recursive case TOWARD the base case.
+
+// Most people understand that a recursive function needs a BASE CASE and a RECURSIVE CASE. The key thing to remember is the RECURSIVE STEP, which moves the function's current state one step closer to the BASE CASE every time that the function calls itself. //! A function is ONLY recursive if it moves toward the base case. Without the recursive step, the function becomes an infinite loop that leads to a STACK OVERFLOW.
+
+// Recursion is very similar to looping except that a function calls itself, instead of using a loop, to accomplish its purpose.
+
+//& Let's write a recursive function to print out the contents of an array.
