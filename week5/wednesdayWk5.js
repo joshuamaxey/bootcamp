e1413d6cb6b01034d1f854472d0fb1b1844aab1f
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-10 13:32:18 -0500

    added more notes on recursion and callstack

--- week5/wednesdayWk5.js
+++ week5/wednesdayWk5.js
@@ -41,3 +41,53 @@ printArray([1, 2, 3, 4, 5]); // Now we can see that when we call this function, 
 //^ 1. base case (a conditional with a return that stops the function);
 //^ 2. expression (code that performs a specific task [such as logging to the console, performing mathematical operations, etc])
 //^ 3. recursive case AND step (function calls itself with incremented/updated arguments)
+
+//* Additionally, notice that ITERATION and RECURSION are both methods of accomplishing the same thing. Any function that can be solved recursively can also be solved iterably. The above printArray function, for instance, is shorter and more readable when we use a for loop. In the real world, I would probably use iteration for this particular purpose because it requires only two lines of code and is more readable.
+
+//& Now lets try a more complicated example: A function multiply(num1, num2) that returns the product of num1 and num2 WITHOUT using *, for, or while.
+
+//^ The first thing we do is understand the problem. Multiplying two numbers is the same thing as adding num1 to itself num2 times. (3 * 5) === (3 + 3 + 3 + 3 + 3);
+
+//! Whenever you are writing a recursive function. start with the BASE CASE. Then the expression. Then the RECURSIVE CASE AND STEP. Remember that we are effectively that num1 is adding itsef to itself num2 times.
+
+function multiply(num1, num2) {
+
+    // return num1 * num2; // Here is how I would solve this problem without recursion.
+
+    if (num2 === 0) return 0; // Any number multiplied by 0 is 0. So if num2 is 0, return 0. This is our BASE CASE.
+
+    if (num2 === 1) return num1; //Any number * 1 is itself. So if num2 = 1, we return num1.
+    if (num2 === 2) return num1 + num1; //num1 * 2 = num1 + num1.
+    if (num2 === 3) return num1 + num1 + num1; //num1 * 3 = num1 + num1 + num1.
+
+    // Here we can see that every time num2 increases by 1, we add ANOTHER num1 to the total. SO you can think of it as num1 + the previous calculation. //^ Note that the if statements above are the "expression" part of the recursive function.
+
+    return num1 + multiply(num1, num2 - 1); // THIS is the RECURSIVE CASE and STEP.
+}
+
+console.log(multiply(5, 5)); // 25
+
+//& Now lets refactor this code. With our recursive step, the only operations we need to EXPLICITLY code for are the those when num2 = 0 and num2 = 1:
+
+function multiply2(num1, num2) {
+
+    if (num2 === 0) return 0;
+    if (num2 === 1) return num1;
+
+    return num1 + multiply2(num1, num2 - 1); // USE THE DEBUGGER AND WATCH HOW THE VARIABLES CHANGE. WE can see that the function calls itself seven times (until we reach the base case), then pops each of the operations off the CALL STACK one at a time, adding 3 to itself each pop until we reach the desired outcome (21). Thus we have accomplished our task using ONLY addition and conditional statements by recursion, no for/while loops OR "*" necessary.
+}
+
+console.log(multiply2(3, 7)); // 21
+
+//& Let's refactor this code ONE more time! If we realize that (num2 === 1) return num1 IS THE SAME AS if (num2 ===1) return 0 + num1, we see that our second conditional is actualy unnecessary because it follows the recursive pattern and will be covered by our "return num1 + multiply2(num1, num2 - 1)" pattern.
+
+function multiply3(num1, num2) {
+    debugger
+    if (num2 === 0) return 0; // BASE CASE
+
+    return num1 + multiply3(num1, num2 - 1); //RECURSIVE CASE and RECURSIVE STEP
+};
+
+console.log(multiply3(6, 5)); // 30
+
+//^ Run the debugger on this function too. Every time that we recursively call this function, that function call is added to the call stack AND the value of num2 decreases by 1. One we reach the BASE CASE, the function returns 0 and then JavaScript begins popping those function calls off of the callstack ONE BY ONE, adding num1 to itself with each pop until the callstack is empty. Once there is nothing left in the call stack, the function returns the value (which is the multiplied value that we want).
