6d9da166c75990a3d09a92a7cb59fb7bf2f47364
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-10 12:05:58 -0500

    Began notes on wednesdayWk5

---
+++ week5/wednesdayWk5.js
@@ -0,0 +1,9 @@
+//! Recursion tips
+
+//^ There are THREE steps that define a recursive function:
+
+//^ 1. The function CALLS itself
+//^ 2 The function has an END STATE (base case)
+//^ 3. The function MOVES CLOSER to the base case with each call.
+
+//^ Also recall: There is a BASE CASE, a RECURSIVE CASE, and a RECURSIVE STEP.
