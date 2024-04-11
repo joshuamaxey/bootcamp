adb21020b6d678f71333dde81e7202490c7610b2
Omega Dziko F. <megaoy@yahoo.com>
2024-04-11 19:37:28 -0400

    5th prb slvd

--- week5/aa03-recursion-pt2/problems/05-reverse.js
+++ week5/aa03-recursion-pt2/problems/05-reverse.js
@@ -12,11 +12,37 @@ reverse("id"); // "di"
 reverse(""); // ""
 ***********************************************************************/
 
-
+debugger
 function reverse(str) {
-  // Your code here 
+
+  let stringArr = str.split('');
+
+  let reverseArr = [];
+
+  if(stringArr.length === 0) {
+
+    return reverseArr.join('');
+
+  }
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //   reverseString += str[i];
+  // }
+  reverseArr.push(stringArr[stringArr.length - 1]);
+
+  stringArr.pop();
+
+
+  return reverseArr.join('') + reverse(stringArr.join(''));
+
+
 }
 
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+console.log(reverse("atom")); // "mota"
+console.log(reverse("q")); // "q"
+console.log(reverse("id")); // "di"
+console.log(reverse("")); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
