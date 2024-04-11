3e7a6e666e278f5ae04879353c3fa0e9a0c625f4
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-11 17:37:36 -0500

    finished problem 04 recursion

--- week5/aa03-recursion-pt1/problems/04-range.js
+++ week5/aa03-recursion-pt1/problems/04-range.js
@@ -12,9 +12,29 @@ range(7, 6); // []
 
 
 function range(start, end) {
-  // Your code here 
+
+  if (start > end) return [];
+
+  let arr = [start];
+
+  if (start === end - 1) {
+    return arr
+  }
+
+  return arr = [...arr, ...range(start + 1, end)];
+
+  // for (let i = start; i < end; i++) {
+  //   arr.push(i);
+  // }
+
+  // return arr;
+
+
 }
 
+console.log(range(1, 5)); // [1, 2, 3, 4]
+console.log(range(3, 4)); // [3]
+console.log(range(7, 6)); // []
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
