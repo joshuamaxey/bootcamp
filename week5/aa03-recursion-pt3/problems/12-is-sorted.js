9f45bc08ca5fd6ab5d25ebd22b92c22e12c66de7
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-13 13:36:36 -0500

    added recursion problems pt4

--- week5/aa03-recursion-pt3/problems/12-is-sorted.js
+++ week5/aa03-recursion-pt3/problems/12-is-sorted.js
@@ -12,9 +12,20 @@ isSorted([5, 4, 3, 2, 1]); // false
 
 
 function isSorted(arr) {
-  // Your code here 
+
+  if (arr.length === 0) return true;
+
+  if (arr[arr.length - 1] < arr[arr.length - 2]) return false;
+
+  arr.pop();
+
+  return isSorted(arr);
 }
 
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
