e88ec6af320d34bedd364b9e1b6ef0eadc54aeb9
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-13 10:56:33 -0500

    added some notes on IIFE

--- week5/saturdayWk5.js
+++ week5/saturdayWk5.js
@@ -17,8 +17,39 @@ const sayHiExpression = function sayHello() {
     console.log("Hello, World!"); //Function expression;
 }
 
+const sayHiAnonymous = function() {
+    console.log("Hello, World!"); //Function expression (anonymous);
+}
+
 function sayHiRegular() {
     console.log("Hello, World!"); //Regular function;
 }
 
 const sayHiArrow = () => console.log("Hello, World!"); //Arrow function;
+
+//^ As you can see, we currently have four different ways to write functions.
+
+sayHiExpression(); // Hello, World!
+sayHiAnonymous(); // Hello, World!
+sayHiRegular(); // Hello, World!
+sayHiArrow(); // Hello, World!
+
+//^ An Immediately-Invoked Function Expression (IIFE, iffy) is a function that is called immediately after it has been defined.
+//^ The typical syntax we use to write an IIFE is the same as writing a regular anonymous function that is wrapped in parenthesis (the grouping operator).
+//^ To invoke the IIFE, you add another pair of closed parenthesis. This is what an IIFE function definition/call looks like:
+
+(function() {
+    console.log("run me immediately!");
+})(); // run me immediately!
+
+//^ Because IIFEs are IMMEDIATELY INVOKED, attempting to assign an IIFE to a variable will return the value of the invoked function, like this:
+
+let IIFE = (function() {
+    return "I'm an IIFE!";
+})();
+
+console.log(IIFE); // I'm an IIFE!
+
+//^ NOTICE that when we assign a variable to an IIFE, it allows us to STORE the value that is produced by the function. The IIFE runs immediately, which is why we print "I'm an IIFE" instead of [anonymous function] when we console.log the variable that we assigned the IIFE to. The function is invoked immediately, and the output is stored in the variable.
+
+console.log(IIFE); // I'm an IIFE!
