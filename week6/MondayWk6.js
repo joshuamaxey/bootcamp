a08e7d79500d1aa9dd9d7799314f8358222b4beb
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-14 18:23:54 -0500

    more notes

--- week6/MondayWk6.js
+++ week6/MondayWk6.js
@@ -1,3 +1,11 @@
+
+
+//! Asynchronous JavaScript
+
+//^ Learning Objectives:
+
+//^ 1. Identify JavaScript as a language that utilizes an EVENT LOOP model
+
 //! The Message Queue and Event Loop
 
 // JavaScript is ASYNCHRONOUS and can handle unpredictable events that occur during our application's runtime. This is what makes JavaScript so dynamic and what enables it to be interactive.
@@ -21,3 +29,26 @@
 // 2. MESSAGE QUEUE: Incoming ASYNCHRONOUS events wait on the queue.
 
 // The queue ensures that events which occurred first will be handled first, and so on. Once the stack is empty, that means the current ttask is complete and the next task can be moved from the queue to the stack for execution. This cycle repeats again and again, which is why it is called the Event LOOP.
+
+//^ 2. Identify JavaScript as a SINGLE-THREADED LANGUAGE
+
+// JavaScript is a SINGLE-THREADED language, which means that it can only perform ONE TASK at a time. This is important because it means that NO TASK IN JAVASCRIPT can be interuupted by ANOTHER TASK.
+
+//^ 3. Describe the difference between ASYNCHRONOUS and SYNCHRONOUS code
+
+
+
+//^ 4. Execute the ASYNCHRONOUS function setTimeout with a callback.
+
+
+
+//^ 5. Given the function, "function asyncy(cb) { setTimeout(cb, 1000); console.log("async") }"
+//^ and the function, "function callback(" {console.log("callback"); }" predict the output of "asyncy(callback);"
+
+
+
+//^ 6. Use setInterval to have a function execute 10 times with(in) a 1-second time-period. After the 10th cycle, clear the interval.
+
+
+
+//^ 7. Write a program that accepts user input using Node's readline module.
