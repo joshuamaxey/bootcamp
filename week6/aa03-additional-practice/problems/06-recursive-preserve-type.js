8e1ad7e5028589a75e95a97fafe8ba84a65943ad
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-22 13:40:49 -0500

    finished problem 06

--- week6/aa03-additional-practice/problems/06-recursive-preserve-type.js
+++ week6/aa03-additional-practice/problems/06-recursive-preserve-type.js
@@ -26,6 +26,8 @@ Note: the mocha tests do not test that your solution is implemented recursively.
 However, for bonus points try to solve it both with recursion and iteration.
 */
 
+//! ======================RECURSIVE==============================
+
 function recursivePreserveType(array) {
     return function filterByType(type) {
         if (array.length === 0) { // base case
@@ -43,14 +45,40 @@ function recursivePreserveType(array) {
     };
 }
 
-const preserveFunc = recursivePreserveType([1, 'one', 2, 'two', 3, 'three']);
-console.log(preserveFunc('number')); // prints [1, 2, 3]
-console.log(preserveFunc('string')); // prints ['one', 'two', 'three']
+//! ======================ITERATIVE==================================
 
-const preserveFunc2 = recursivePreserveType([2, undefined, 'world', { color: 'red' }, true, 3, [4, 5], 'hello', false]);
-console.log(preserveFunc2('number')); // prints [2, 3]
-console.log(preserveFunc2('object')); // prints [ { color: 'red' }, [4, 5] ]
-console.log(preserveFunc2('boolean')); // prints [ true, false ]
+function iterativePreserveType(array) {
+
+    return function(...types) { // return a function that takes in any number of 'types' as strings
+
+        const result = []; // initialize an empty array to hold the filtered elements.
+
+        for (el of array) { //for each element of the array
+            if (types.includes(typeof el)) { //if the current element is of the specified type
+                result.push(el); // push that element to the 'result' array.
+            }
+        }
+        return result; // retur the 'result' array.
+    }
+}
+
+// const preserveFunc = recursivePreserveType([1, 'one', 2, 'two', 3, 'three']);
+// console.log(preserveFunc('number')); // prints [1, 2, 3]
+// console.log(preserveFunc('string')); // prints ['one', 'two', 'three']
+
+// const preserveFunc2 = recursivePreserveType([2, undefined, 'world', { color: 'red' }, true, 3, [4, 5], 'hello', false]);
+// console.log(preserveFunc2('number')); // prints [2, 3]
+// console.log(preserveFunc2('object')); // prints [ { color: 'red' }, [4, 5] ]
+// console.log(preserveFunc2('boolean')); // prints [ true, false ]
+
+const preserveFuncIterative = iterativePreserveType([1, 'one', 2, 'two', 3, 'three']);
+console.log(preserveFuncIterative('number')); // prints [1, 2, 3]
+console.log(preserveFuncIterative('string')); // prints ['one', 'two', 'three']
+
+const preserveFunc2Iterative = iterativePreserveType([2, undefined, 'world', { color: 'red' }, true, 3, [4, 5], 'hello', false]);
+console.log(preserveFunc2Iterative('number')); // prints [2, 3]
+console.log(preserveFunc2Iterative('object')); // prints [ { color: 'red' }, [4, 5] ]
+console.log(preserveFunc2Iterative('boolean')); // prints [ true, false ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
