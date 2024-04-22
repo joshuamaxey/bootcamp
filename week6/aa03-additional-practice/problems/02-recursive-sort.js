e48a7c646de6a9dc9cc302cfe5f0751d9ac944bb
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-21 19:37:57 -0500

    worked on problems

--- week6/aa03-additional-practice/problems/02-recursive-sort.js
+++ week6/aa03-additional-practice/problems/02-recursive-sort.js
@@ -9,9 +9,86 @@
     console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 */
 
-function sort(nums) {
-    // Your code here 
+// function sort(nums) {
+//     let newArr = [];
+
+//     if (nums.length === 0) {
+//         return nums;
+//     }
+
+// }
+
+//! =========================================================================
+
+// function merge(left, right) {
+//     const result = [];
+
+//     let leftIndex = 0;
+//     let rightIndex = 0;
+
+//     while (leftIndex < left.length && rightIndex < right.length) {
+//         if (left[leftIndex] < right[rightIndex]) {
+//             result.push(left[leftIndex]);
+//             leftIndex++;
+//         } else {
+//             result.push(right[rightIndex]);
+//             rightIndex++;
+//         }
+//     }
+//     return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+// };
+
+// function mergeSort(arr) {
+//     if (arr.length <= 1) { //base case
+//         return arr;
+//     }
+
+//     const middle = Math.floor(arr.length / 2);
+//     const left = arr.slice(0, middle);
+//     const right = arr.slice(middle);
+
+//     return merge(mergeSort(left), mergeSort(right));
+// };
+
+// function sort(arr) {
+//     return mergeSort(arr);
+// }
+
+//! ===========================================================================
+
+// function sort(arr) {
+//     return arr.slice().sort((a, b) => a - b);
+//     // Here, we use the .slice() method with no parameters to create a shallow copy of the array. This provides a copied array for us to sort so that the original remains unmutated (since the problem asks for a NEW array)
+//     // Then we use the .sort() method with a compare function to sort the elements in ascending order.
+// }
+
+//! ===========================================================================
+
+function sort(arr) {
+    // debugger
+    if (arr.length <= 1) { //BASE CASE
+        return arr;
+    }
+
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            rightpush(arr[i]);
+        }
+    }
+
+    return sort(left).concat(pivot, sort(right));
 }
+
+console.log(sort([])) // prints []
+console.log(sort([9])) // prints [9]
+console.log(sort([5, 4, 3, 2, 1])) // prints [1, 2, 3, 4, 5]
+console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
--- week6/aa03-additional-practice/problems/03-even-number-range.js
+++ week6/aa03-additional-practice/problems/03-even-number-range.js
@@ -1,6 +1,6 @@
 /* Even Number Range
 
-    Write a function that accepts two numbers. 
+    Write a function that accepts two numbers.
     The function should return an array of all even numbers between the two arguments inclusively.
 
     console.log(evenNumberRange(-3, 2)); // prints [-2, 0, 2]
@@ -8,9 +8,43 @@
     console.log(evenNumberRange(2, 0)); // prints []
 */
 
+//! ====================================ITERATIVE======================================
+
+// function evenNumberRange(num1, num2) {
+//     let arr = [];
+
+//     for (let i = num1; i <= num2; i++) {
+//         if (i % 2 === 0) {
+//             arr.push(i);
+//         }
+//     }
+
+//     return arr;
+// }
+
+//! ===================================RECURSIVE=======================================
+
 function evenNumberRange(num1, num2) {
-    // Your code here 
+    let evenArr = [];
+
+    if (num1 <= num2) {
+        if (num1 % 2 === 0) {
+            evenArr.push(num1);
+        }
+
+        const nextNum = num1 + 1;
+
+        const result = evenNumberRange(nextNum, num2);
+
+        evenArr = evenArr.concat(result);
+    }
+
+    return evenArr;
 }
+
+console.log(evenNumberRange(-3, 2)); // prints [-2, 0, 2]
+console.log(evenNumberRange(22, 25)); // prints [22, 24]
+console.log(evenNumberRange(2, 0)); // prints []
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
