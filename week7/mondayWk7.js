9e1e984b830aa4a38441f4f72f1b04a73a8cd007
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-23 18:37:47 -0500

    added week7

---
+++ week7/mondayWk7.js
@@ -0,0 +1,104 @@
+//! Problem-Solving, OOP, and TDD
+
+//! Remember POLYA!
+
+//^ 1. understand the problem
+//^ 2. Come up with a plan
+//^ 3. Execute the plan
+//^ 4. Revisit and refactor your solution
+
+// If you're stuck on execution, revisit the plan.
+// If you can't come up with a plan, make sure you understand the problem.
+// If you can't understand the problem, or do but can't come up with a plan, work with your partner and come up with a great coding question
+
+//! Great coding questions are:
+
+//^ 1. Specific
+//^ 2. Clear and concise
+//^ 3. Show you put work into it
+
+// Teamwork and communication are essential to success.
+
+//! Object-Oriented Programming (OOP) and Test-Driven Development (TDD)
+
+// OOP is a programming paradigm that promotes MODULARITY by enabling developers to divide their code into smaller, self-contained units called OBJECTS which represent soething in the real world and allow us to organize our code into modular, interchangeable components. To practice OOP, you will:
+
+//^ build simple JavaScript terminal games.
+//^ learn how to read and write tests using a comprehensive test suite
+//^ Import a rendering library to display your games and interact with it vi an APPLICATION PROGRAM INTERFACE (API).
+//^ Learn how to manage CONTEXT in JavaScript objects. This is one of the trickiest parts of the language, and familiarity with context is requird to unlock JavaScript's full potential.
+
+//! Asking Great Coding Questions
+
+//^ Due Diligence
+
+//Before th 15-minute rule, make sure you do the following when you're stuck on a coding problem:
+
+// 1. Read error messags and logs to isolate bugs.
+// 2. Look up error messages
+// 3. Review relevant sections of Readings
+// 4. Check official documentation
+// 5. Discuss with your peer(s)
+
+// If you're still stuck after this, it's time for the 15-minute rule.
+
+//^ The 15-Minute Rule
+
+// The 15-minute Rule is meant to help you come up with a GREAT question in 15 minutes and involves 4 steps:
+
+// 1. Write down evrything you DO understand about the problem.
+// 2. Write down everything you DON'T understand about the problem.
+// 3. Based on 1 and 2, write down a SPECIFIC question.
+// 4. Clean up the question by ELIMINATING unnecessary words.
+
+//! Summary
+
+// Whenever you're stuck on a problem:
+
+//^ 0. POLYA
+
+// 1. understand the problem
+// 2. Come up with a plan
+// 3. Execute the plan
+// 4. Revisit and refactor your solution
+
+//^ 1. Due Diligence to develop a partial understanding of the problem.
+
+// 1. Read error messags and logs to isolate bugs.
+// 2. Look up error messages
+// 3. Review relevant sections of Readings
+// 4. Check official documentation
+// 5. Discuss with your peer(s)
+
+//^ 2. 15-minute rule to develoop a great coding question.
+
+// 1. Write down evrything you DO understand about the problem.
+// 2. Write down everything you DON'T understand about the problem.
+// 3. Based on 1 and 2, write down a SPECIFIC question.
+// 4. Clean up the question by ELIMINATING unnecessary words.
+
+//^ 3. Teamwork and communication.
+
+//~ Intro to Programming
+
+//~ OBject-Oriented Programming is CRITICAL.
+
+//~ To be successful, FOCUS on the curriculum, ENGAGE in teamwork, AVOID USING AI TO LEARN.
+
+//~ DRY - DONT REPEAT YOURSELF
+
+//~ SINGLE RESPONSIBILITY - Let each function/block have only ONE responsbility - use helper functions, etc.
+
+//! Clean Maintainable Code Learning Objectives
+
+//^ Export and Import elements of code from one file to another, including code written by others
+//^ Write DRY code
+//^ Utilize DRY to improve code written by another
+//^ Construct Classes and FUnctions that adhere to the SIngle Responsbility Principle
+//^ Identify violations of the Single Responsibility PRinciple (SRP) in code and refactor it
+//^ Utilize an API to interact with functionality found in a li brary
+//^ Write functiosn to abstract complex API commands into simpler-to-use tools
+
+//! Reading Tests
+
+// The most important things about a test are that it is READABLE and UNDERSTANDABLE
