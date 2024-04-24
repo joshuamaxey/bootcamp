67022e55e55f9ab1ef1327299e873f9c83e2af0f
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-24 15:37:02 -0500

    More tuesday Notes

--- week7/tuesdayWk7.js
+++ week7/tuesdayWk7.js
@@ -138,17 +138,19 @@ const pencil = {
 
 // Consider teh following book object:
 
-class Book {
-    constructor(title, series, author) {
-        this.title = title;
-        this.series = series;
-        this.author = author;
-    }
+//~ Commented out for use later in the notes
 
-    getInformation() {
-        return `${this.title} by ${this.author}`;
-    }
-}
+// class Book {
+//     constructor(title, series, author) {
+//         this.title = title;
+//         this.series = series;
+//         this.author = author;
+//     }
+
+//     getInformation() {
+//         return `${this.title} by ${this.author}`;
+//     }
+// }
 
 //^ Notice that class names begin with a capital letter.
 
@@ -161,13 +163,15 @@ class Book {
 
 //^ Use the 'new' keyword to create or INSTANTIATE an instance of a class:
 
-const fellowshipOfTheRing = new Book (
-    "The Fellowship of the Ring",
-    "The Lord of the Rings",
-    "J.R.R. Tolkien"
-);
+//~ commented out for use later in notes
 
-console.log(fellowshipOfTheRing)
+// const fellowshipOfTheRing = new Book (
+//     "The Fellowship of the Ring",
+//     "The Lord of the Rings",
+//     "J.R.R. Tolkien"
+// );
+
+// console.log(fellowshipOfTheRing)
 
 // Three things happen when instantiating an instance of a class:
 
@@ -181,24 +185,22 @@ console.log(fellowshipOfTheRing)
 
 // Methods are typically invoked on an instance of a class that is stored in a variable. Thus, they are often called INSTANCE METHODS. //! See the 'Book' class above, and the 'getInformation' method specified within.
 
-console.log(fellowshipOfTheRing.getInformation()); // prints 'The Fellowship of the Ring by J.R.R Tolkien.
+// console.log(fellowshipOfTheRing.getInformation()); // prints 'The Fellowship of the Ring by J.R.R Tolkien.
 
 //! Using the instanceof operator to check an object's type.
 
-console.log(fellowshipOfTheRing instanceof Book); // True
+// console.log(fellowshipOfTheRing instanceof Book); // True
 
-//! Writing Classes Demo =====================================================================
+//! ============================== Writing Classes Demo =============================================
 
-//^ Classes are TEMPLATES FOR CREATING OBJECTS. They encapsulate data together with the code that works on that data.
-
-class Scripture {
-    constructor(text, book, number) {
+class Scripture { // Classes are TEMPLATES FOR CREATING OBJECTS. They encapsulate data together with the code that works on that data.
+    constructor(text, book, number) { // Constructor turns raw data into properies - It builds and organizes our data.
         this.text = text;
-        this.book = book;
+        this.book = book; // The THIS keyword allows us to maintain the context of a specific instance when we call on our isntance methods.
         this.number = number;
     }
 
-    getVerse() {
+    getVerse() { // Instance methods allow us to interact with our data once the object is built.
         return `${this.text} - ${this.book} ${this.number}`
     }
 }
@@ -212,3 +214,237 @@ const StrongAndCourageous = new Scripture (
 console.log(StrongAndCourageous);
 
 console.log(StrongAndCourageous.getVerse());
+
+//! Class Syntax 1 Quiz
+
+class Person {
+    constructor(firstName) {
+        this.firstName = 'Caleb';
+    }
+
+    greet() {
+        console.log(`${this.firstName} says hello!`);
+    }
+}
+
+const kristen = new Person('Kristen');
+
+console.log(kristen.firstName) // should return 'Caleb'
+console.log(Person.firstName); // should return 'Caleb' //! Wrong. Returned 'undefined'
+
+//! Inheritance
+
+// In additiona to ENCAPSULATION, OOP relies on INHERITANCE, where classes receive teh properties and methods of their parent or parents.
+
+//! Inheritance Class Syntax
+
+class MyClass {}; // This class is, by default, a child of the 'Object' class.
+
+class MyChildClass extends MyClass {} // MyChildClass is a child of MyClass, which is its parent class.
+
+//^ When a child class extends, or inherits from, a parent class, it inherits the methods of the parent class. This means that you can use all the methods defined in the parent class within the ethods of the child class.
+
+class Animal {
+    constructor(name, sound) {
+        this.name = name;
+        this.sound = sound;
+    }
+
+    speak() {
+        console.log(`${this.name} goes ${this.sound}!`);
+    }
+}
+
+class Dog extends Animal {
+    //constructor is inherited from Animal.
+}
+const fluffy = new Dog("Fluffy", "woof");
+
+fluffy.speak(); // Fluffy goes woof!
+
+// Lets say that you want to create a Cat cass and want all cats to have a 'meow' sound. You can create a constructor in the Cat class using the name parameter. Inside of the Cat constructor, you call the constructor of the Animal class using the super function and pass in the name parameter long with the 'meow' sound.
+
+class Cat extends Animal {
+    constructor(name) {
+        super(name, 'meow');
+    }
+}
+
+const butthead = new Cat('Butthead');
+
+butthead.speak(); // Butthead goes meow!
+
+// Now you instantiate a new Cat with just a name. THe sound will be 'meow' for every cat.
+
+const leo = new Cat('Leo');
+
+leo.speak(); // Leo goes meow!
+
+//! ======================================== Static Methods and Variables =========================================
+
+//^ What are static methods and variables?
+//^ How do static methods and variables differ from instances?
+//^ How do we declare stati methods and variables?
+
+//! Static Methods
+
+// Static methods are invoked direclty on a class (they are called CLASS METHODS);
+// Define a static method using the STATIC keyword;
+
+class Book {
+    constructor(title, series, author) {
+        this.title = title;
+        this.series = series;
+        this.author = author;
+
+        Book.numBooks++ // numBooks will be incremented each time the constructor method is run (whenever a new Book instance is created)
+    }
+
+    getInformation() {
+        return `${this.title} by ${this.author}`;
+    }
+
+    static getTitles(...books) {
+        return books.map((book) => book.title);
+    }
+
+    static numBooks = 2; // Every time we create a new book, this count increases.
+}
+
+const fellowshipOfTheRing = new Book (
+    "The Fellowship of the Ring",
+    "The Lord of the Rings",
+    "J.R.R. Tolkien"
+);
+
+const theTwoTowers = new Book(
+    "The Two Towers",
+    "The Lord of the Rings",
+    "J.R.R. Tolkien"
+);
+
+const bookTitles = Book.getTitles(fellowshipOfTheRing, theTwoTowers);
+
+console.log(bookTitles); // prints ["The Fellowship of the Ring", "The Two Towers"]
+
+//^ Notice that the Math functions (Math.random, Math.max, Math.floor, etc) are all called starting with the "Math" class name. This is because the Math object actually consists fo ONLY static methods. The Math.random, Math.max, etc functions are called directly on the Math class object, not on an instance.
+
+//! Static Variables
+
+// Static variables (or properties) are accessed directly from the class. They are declared like static methods using the static keyword vollowed by the variable name and value assignment.
+
+// Static variables are useful for caching information about the class, configurations of the class, or other data that you don't nee to repicate across instances.
+
+// Consider the static variable numBooks in the Book class above ^^
+
+//^ Static variables are variables that we DON'T need to store on every instance.
+
+//! Class Syntax 2 Quiz
+
+class Task {
+    static taskList = [];
+
+    static printTaskList() {
+        console.log(this.taskList);
+    }
+
+    static addTask(task) {
+        this.taskList.push(task);
+    }
+
+    constructor(taskDetail) {
+        this.taskDetail = taskDetail;
+        Task.addTask(this);
+    }
+}
+
+const laundry = new Task('laundry');
+const dishes = new Task('dishes');
+
+// Task.printTaskList();
+
+// laundry.printTaskList();
+
+// laundry.taskList;
+
+//! Class Inheritance Demo
+
+// Class inheritance is when one class receives properties and methods from another.
+
+// We use class inheritance to keep our code DRY so that your code can be re-used and doesn't need to be written multiple times.
+
+// You make a class inherit from another using the 'extends' keyword.
+
+// You use the 'super' keyword when you want to pass a part of your constructor from the parent to the child class.
+
+//! Class Inheritance Quiz
+
+class Fruit {
+    constructor(color, taste) {
+        this.color = color;
+        this.taste = taste;
+    }
+
+    eat() {
+        console.log(`You ate a(n) ${this.constructor.name}.`);
+    }
+}
+
+class Apple extends Fruit {
+    constructor() {
+        super('red', 'sweet');
+    }
+}
+
+const apple = new Apple();
+const orange = new Fruit ('orange', 'tangy');
+
+console.log(apple.color);
+
+apple.eat();
+orange.eat();
+
+//! ===================================== Polymorphism ======================================
+
+//^ Polymorphism is the practice of procesing multiple data types and class through a single interface.
+
+//^ Two commonn types of polymorphism are 'overloading' and 'overriding'
+
+// Consider the 'toString()' method of the OBject class. Since evvery object in JavaScript is a child class (or grandchild, or great-grandchild, etc) class of the original Object class, that means that every object in JavaScript has a toString() method.
+
+//* To try it out, open a terminal, start node, adn type the following:
+
+// > [1, 2, 3].toString();
+// '1,2,3'
+// > "some text".toString();
+// 'some text'
+// > new Date().toString();
+// '«the current date and time»'
+// > new Object().toString();
+// [object Object]
+
+// The toString() method does different things depending on which data type it is applied to.
+
+// Arrays: Takes the value in the array and turns the into a comma-delimited string.
+// String: Does nothing and returns the string object.
+// Date: returns a long textual represeentation of the date and time, which is represented by the Date object.
+// Object: returns "[object Object" because that's all it knows about itself in this case.
+
+//! In Summary:
+
+//^ Polymorphism is a way of extending the functionality of a given method within a child class to do something more or something different than the method within its parent class.
+
+//! Polyomrphism Demo
+
+//^ There are TWO reasons to use polymorphism in programming.
+
+// 1. FUNCTION OVERLOADING: When a function needs to have different behavior based on its input. Maybe we need to accept different input types (numbers vs strings using the "+" operator). Maybe we need to accept different numbers of inputs (Maybe we want to do X when we have one number of inputs, or Y when we have no inputs, etc). Or maybe we need the function to perform different logic based on the input.
+
+// 2. FUNCTION OVERRIDING: Because of class inheritance, a parent passes on all of its methods and properties and variables to its child classes. Maybe that child doesn't need to have exactly the same functionality with respect to one of the methods that it has inherited. In this case, we would say that we want to OVERRIDE that method. Consider a 'findArea()' method of a parent class called 'Shape'. The 'Square' and 'Circle' classes both inherit from 'Shape', but need to implement the 'findArea()' method differently.
+
+//^ So how do we override an inherited function or method?
+
+// Declare a method with the same name as the method within the parent class.
+// Write the new method.
+
+//! Polymorphism Quiz
