bbbbe88157d657163251564e9e38f90a34ca2f04
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-29 19:36:40 -0500

    finished phase 05 of context long practice

--- week8/context/problems/05-bind-to-an-arg.js
+++ week8/context/problems/05-bind-to-an-arg.js
@@ -1,6 +1,10 @@
 function bindToAnArg(func, arg) {
-  // Your code here 
+  return func.bind(func, arg)
 }
+
+//! Done!
+
+//^ This one was a little bit confusing. we return a NEW function (because the bind() method always returns a new function) with the context bound to that function itself and with the argument set to the argument that was provided within the bindToAnArg function.
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = bindToAnArg;
