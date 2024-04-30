b064834a0a4fb2aa6c0d16aa9732433b1a08af9d
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-29 19:50:05 -0500

    finished phase 06 of context long practice

--- week8/context/problems/06-fancy-calculator.js
+++ week8/context/problems/06-fancy-calculator.js
@@ -1,4 +1,32 @@
-// Your code here 
+const Calculator = require("./02-calculator")
+
+class FancyCalculator extends Calculator {
+
+	setTotal(newTotal) {
+		return this.total = newTotal;
+	}
+
+	modulo(num) {
+		return this.total %= num
+	}
+
+	squared() {
+		return this.total *= this.total
+	}
+}
+
+let fancyCalculator = new FancyCalculator();
+console.log(fancyCalculator.setTotal(5)); // => 5
+console.log(fancyCalculator.squared());   // => 25
+console.log(fancyCalculator.modulo(4));   // => 1
+console.log(fancyCalculator.total)        // => 1
+
+// can use instance methods on the Calculator class
+console.log(fancyCalculator.add(9))       // => 10
+
+//^ Just spent a while trying to figure out why I was printing undefined for these new methods, attempting to apply the bind method etc, then realized that I never used the 'return' keyword on any of them. Added the return keyword and now they work.
+
+//! Done!
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
