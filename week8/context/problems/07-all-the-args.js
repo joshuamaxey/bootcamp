ca66c98edd1ba165260f7928e7a45004949d4d92
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-30 13:26:15 -0500

    finished phase 07 context long practice

--- week8/context/problems/07-all-the-args.js
+++ week8/context/problems/07-all-the-args.js
@@ -1,6 +1,32 @@
 function allTheArgs(func, ...args) {
-  // Your code here 
+  return func.bind(func, ...args)
 }
+
+function sum(...nums) {
+  return nums.reduce((acc, num) => acc + num);
+}
+
+const onePlusTwo = allTheArgs(sum, 1, 2);
+
+const onePlusTwoPlusThree = onePlusTwo(3);
+const onePlusTwoPlusFour = onePlusTwo(4);
+
+console.log(onePlusTwoPlusThree); // => 6
+console.log(onePlusTwoPlusFour);  // => 7
+
+const bow = (...names) => {
+  return "You bowed to " + names.join(" and ");
+};
+
+console.log(bow("Sandy")) // prints "You bowed to Sandy"
+
+let bowSandy = allTheArgs(bow, "Sandy");
+console.log(bowSandy()); // prints "You bowed to Sandy"
+console.log(bowSandy("Joe", "Nico")); // prints "You bowed to Sandy and Joe and Nico"
+
+//^ Here, we call the function using the bind() method, binding the function itself as its own context and then specifying the arguments that were provided in the allTheArgs function.
+
+//! Done!
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
