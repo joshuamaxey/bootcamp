cea105d4f57f89b41b12f271e55fff114b709bcc
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-29 19:33:22 -0500

    finished phase 04 context long practice

--- week8/context/problems/04-change-context.js
+++ week8/context/problems/04-change-context.js
@@ -1,6 +1,21 @@
 function changeContext(func, obj) {
-  // Your code here 
+  return func.call(obj)
 }
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen)); // => Kristen
+
+//! Done!
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
