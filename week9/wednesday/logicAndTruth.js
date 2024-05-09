1e51d39b8c0579f09ab309b4f0ab2e1ac38d6203
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-05-09 17:25:25 -0500

    more notes & practices

---
+++ week9/wednesday/logicAndTruth.js
@@ -0,0 +1,8 @@
+//^ Construct the truth table for core logic expressions
+    // and, or, not, xor
+//^ Construct a truth table for compound logic expressions
+//^ Translate between base-10 (decimal), base-2 (binary), and base-16 (hexadecimal) integers
+//^ Translate between hexadecimal and ASCII
+//^ Define bits, bytes, kilobytes, megabytes, and gigabytes
+
+//! Logic and truth
---
+++ week9/wednesday/scratch.js
@@ -0,0 +1,6 @@
+// toString(radix) takes a number and converts it from one base to another
+// parseInt(string, radix) takes a given string and converts it to an integer (base 10) using the specified radix
+
+function decimalToBinary(blob) {
+    return blob.toString(2); // 2 means convert to base 2
+}
