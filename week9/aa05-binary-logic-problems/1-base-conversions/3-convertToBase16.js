ae4b7f5853d8a4846ffda343c2a8c80db9996940
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-05-17 19:29:56 -0500

    finished 3-convertToBase16

--- week9/aa05-binary-logic-problems/1-base-conversions/3-convertToBase16.js
+++ week9/aa05-binary-logic-problems/1-base-conversions/3-convertToBase16.js
@@ -3,7 +3,12 @@
 /******************************************************************************/
 
 const convertToBase16 = element => {
-  // Your code here 
+  if (typeof element === "number") { // Check if 'element' is a number
+    return "0x" + element.toString(16); // If it is, use the .toString(16) method to convert the decimal number to its hexadecimal equivalent (as a string), then prefix it with the hexadecimal prefix "0x"
+  } else { // Otherwise, we assume that element is a binary (base-2) number (in string form)
+    const base10 = parseInt(element.substring(2), 2); // If that's the case, use the parseInt() method with substring(2) to remove the prefix from the binary number and then convert the base-2 (binary) value to its decimal (base-10) equivalent.
+    return "0x" + base10.toString(16); // Then we use the base10.toString(16) method to convert the decimal (base-10) number to its base-16 (hexadecimal) equivalent and then prefix it with the hexadecimal "0x" prefix.
+  }
 };
 
 /******************************************************************************/
