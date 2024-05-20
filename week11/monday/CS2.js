75bb526e85d5ea8e8a339395724ce8e0976b5f30
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-05-20 18:43:56 -0500

    moved monday note files to monday directory in week11

---
+++ week11/monday/CS2.js
@@ -0,0 +1,54 @@
+//! Intro
+
+// CS1 was about measuring code performance and learning how to choose the right data structures with which to write clean, efficient code.
+
+// We focused on arrays, linked lists, and hash tables.
+
+//^ Now we will learn to implement algorithms, travers multi-dimensional data sets, and solve linear problems in sub-linear tie.
+
+//^ We will use search, sorting, and traversal algorithms.
+
+//^ We will implement coding fundamentals like for loops, nested loops, data swaps, recursion, memory pointers, arrays, stacks, queues, hash tables, sets, 2D arrays, and more.
+
+// This will require creativity.
+
+// We will also explore the foundational structures of the internet.
+
+//^ HMTL is a tree, social media networks are graphs, databases are indexed using binary search trees, and so on.
+
+//^ We will learn to navigate data newtworks to extrapolate insights.
+
+//^ These techniques are at the foundation of social media, recommendation engines, map routing, path-finding, game AI, the entire internet, and more.
+
+//! Problem Solving
+
+// Every problem can be solved with Polya's problem-solving framework. In the following lessons we'll focus specifically on parts 2 and 3 of the problem-solving framework.
+
+//  1. Understand the problem
+//^ 2. Come up with a plan
+//^ 3. Execute the plan
+//  4. Iterate and improve your solution
+
+// Understanding coding problems is largely a matter of careful reading and building a strong grasp of the fundamentals you've learned in the previous units.
+
+// Then you build your skills at coming up with plans.
+
+// We will do this by studying classic algorithms. We will learn many ways to solve a simple problem like sorting array values in numerical order.
+
+// For most of the problems in this unit, we will not be building our own plans. The outlines will be provided. This will allos us to focus our attention on understanding and execution.
+
+// And there will be opportunities to optimize.
+
+//! Asking Quesions
+
+// These are classic algorithms, available online. We don't want to memorize or copy/paste them, but to understand them and then implement them based on that understanding.
+
+// As always, it is important to ask questions for clarity. The characteristics of a good question are:
+
+//^ 1. It's specifically
+
+//^ 2. It's clear and concise
+
+//^ It shows you've put work into it
+
+// This unit also includes a strong emphasis on collaboration and discussion. Make them most of them by asking great questions.
