c4c91ae88d53eba1f7a3f62b7ab6925aa37dcf45
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-05-28 17:45:59 -0500

    began notes on graphs

---
+++ week12/monday/graphs.js
@@ -0,0 +1,35 @@
+//! Objecties
+
+// 1. Describe Graph Terminology
+        // Nodes/vertices and edges
+        // directed vs undirected
+        // cyclic vs acyclic
+        // weighted vs unweighted
+// 2. Implement a graph using an adjacency list
+// 3. Iplement a graph using a matrix
+// 4. Traverse and Search a Graph in both Depth and Breadth-First order
+// 5. Determine whether a node is reachable from another node via a traversal
+// 6. Determine the shortest path between two nodes using a breadth-first search
+// 7. Identify when a whiteboard problem can be solved with a graph
+
+//! Intro to Graphs
+
+//^ We've learned about node-and-pointer data structures including linked lists, binary search trees, and binary trees.
+
+// These are all forms of graphs, which is the most general form of connected data structure
+
+//^ Graphs are an abstract data type represented as a collection of nodes and edges.
+
+// Each node represents one or more values and contains any number of edges pointing to other nodes.
+
+// Graphs represent RELATIONSHIPS between objects.
+
+// Since there are many types of relationships, there are many different types of graphs.
+
+//^ Graphs make use of a lot of the computer science concepts we've learned.
+
+// To solve graph problems, we will make use of arrays, hash tables and sets, stacks and queues, recursion, breadth and depth-first search, and more.
+
+// Graph problems are tricky if you're shaky on any of these concepts. THey show up in advanced coding interviews.
+
+//
