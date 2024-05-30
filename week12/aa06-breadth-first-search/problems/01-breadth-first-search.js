968337f6ba60f57ec6df2450903b1a27199cc096
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-05-30 13:25:13 -0500

    finished breadth-first-search practice

--- week12/aa06-breadth-first-search/problems/01-breadth-first-search.js
+++ week12/aa06-breadth-first-search/problems/01-breadth-first-search.js
@@ -16,12 +16,30 @@ const adjList = {
 }
 
 function breadthFirstSearch(start, end) {
-  // Your code here 
+
+  const queue = [start]; // initialize a queue and enqueue the starting node by default. Remember to use push() to add to the BACK of the queue and shift() to remove from the front, like a line at the store
+
+  while (queue.length > 0) { // While the queue is not empty...
+
+    const current = queue.shift(); // ...shift the first element from the FRONT of the queue, then store it in the variable 'current'
+
+    if (current === end) { // If the current node is our end node...
+
+      return true; // ...return true. Otherwise...
+    }
+
+    for (let neighbor of adjList[current]) { // ...for each neighbor of the current node in the graph...
+
+      queue.push(neighbor); // ...push those neighbors to the queue
+    }
+  }
+
+  return false; // If we traverse the entire graph without finding the end node, then there is no path to that node. Return false.
 }
 
-// console.log(breadthFirstSearch(1, 3)); // -> true
-// console.log(breadthFirstSearch(4, 1)); // -> true
-// console.log(breadthFirstSearch(6, 1)); // -> false
+console.log(breadthFirstSearch(1, 3)); // -> true
+console.log(breadthFirstSearch(4, 1)); // -> true
+console.log(breadthFirstSearch(6, 1)); // -> false
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
