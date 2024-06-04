82bb764e7fab84b10629984fc93dd68c3787b472
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-06-04 14:09:38 -0500

    test

---
+++ remasterySortingTreesGraphs/builtInSort.js
@@ -0,0 +1,13 @@
+//! JavaScript's built-in Array.prototype.sort()
+
+// JavaScript's built-in .sort() method sorts the elements of an array in-place, then returns the sorted array.
+
+// The default sort order is ascending, built upon converting the elements into strings then comparing their sequences of UTF-16 code unit values.
+
+// The time and space complexity of he sort cannot be guaranteed, it depends on implementation.
+
+// The sort method takes in two arguments (a, b) and a compare function.
+
+// If a compareFunction is provided, all non-undefined array elements are sorted according to the return value of the compare function
+
+// All undefined elements ara sorte to the nd of th array, with no call to the compareFunction
--- remasterySortingTreesGraphs/quickSort.js
+++ remasterySortingTreesGraphs/quickSort.js
@@ -1,1 +1,67 @@
 //! Quick Sort
+
+// Like mergeSort(), quickSort() also uses a divide and conquer strategy.
+
+//^ The logic underlying quickSort() is:
+
+    // It is easy to sort elements of an array relative to a particular target value
+    // An array of length 1 or 0 is already (trivially) sorted
+
+//^ What is the time complexity of quickSort();
+
+    // The best and average case runtime for quickSort() is O(n log n)
+
+    // The worst-case runtime for quickSort() when the partitioning is very unbalanced
+
+function quickSort(arr, low, high) {
+
+    // choose an element called 'pivot'
+
+    // take two variables to point left and right of the list excluding the pivot
+
+    // let the left point to the low index
+
+    // let teh right point to the high index
+
+    // While left is less than pivot, move right
+
+    // While right is greater than pivot, move left
+
+    // If both previous conditions are false, swap left and right
+
+    // If left >= right, that value beomes the new pivot
+
+    // Repeat recursively
+
+    if (low < high) {
+
+        let pivot = partition(arr, low, high); // partition the array
+
+        quickSort(arr, low, pivot - 1); // reursively apply the same logic to the left and right arrays
+
+        quickSort(arr, pivot + 1, high);
+    }
+
+    return arr; // return the sorted array
+}
+
+function partition(arr, low, high) {
+
+    let pivot = arr[high]; // choose the right-most element as the pivot
+
+    let i = low - 1; // create a pointer for the greater element
+
+    for (let j = low; j < high; j++) { // iterate through every element
+
+        if (arr[j] <= pivot) { // compare each element to the pivot
+
+            i++; // increment the index each iteration
+
+            [arr[i], arr[j]] = [arr[j], arr[i]]; // if an element smaller than pivot is found, swap it with the greater element
+        }
+    }
+
+    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]]; // swap the pivot element with the greater element specified by i
+
+    return i + 1; // return the position from the partition location
+}
