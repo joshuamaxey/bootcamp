5ea7c4835fcd08fe3b67a09cd053c663fa70724f
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-06-04 17:39:34 -0500

    added file for great coding questions and intro to OOP and TDD

---
+++ oopAndTdd/intro/greatCodingQuestions.js
@@ -0,0 +1,49 @@
+//! The 15-Minute Rule
+
+// There are 5 traits of a great question and 4 steps to come up with one using the 15-minute rule
+
+//! What Makes a Good Question?
+
+//^ When you get stuck on a problem, always begin with due diligence:
+
+    // 1. Read error messages and logs to isolate bugs
+    // 2. Look up error messages
+    // 3. Review relevant sections of readings
+    // 4. Check official documentation
+    // 5. Discuss with a peer
+
+//! The 15-Minute Rule
+
+// The 15-minute rule has two sides: 1. If you're stuck for more than 15 minutes, ask a question. 2. The 15-minute rule helps you come up with a great question in 15 minutes.
+
+//^ These are the traits of a good coding question:
+
+    // 1. It's specific
+    // 2. It's clear and concise
+    // 3. It shows you've put work into it
+
+//^ To go from good to great, your question must also have the following characteristics:
+
+    // 4. It can be answered by an instructor with little effort
+    // 5. The answer unblocks you
+
+// Steps 1, 2, and 3 are the key to achieving steps 4 and 5
+
+//! Put Work into the Question
+
+//^ The 15-minute rule involves 4 steps:
+
+    // 1. Write down everything you DO understand about the problem
+    // 2. Write down everything you DON'T understand about the problem
+    // 3. based on 1 and 2, write down a specific question
+    // 4. clean up your question by eliminating unnecessary words
+
+// After due diligence (step 0), you will have a partial understanding of the problem.
+
+// Once you identify the most significant gaps of confusion, write down a question to address them as specifically as possible.
+
+// Finally, go back through your question and eliminate all unnecessary words.
+
+//! Summary
+
+// We have learned to use the 15-minute rule to come up with great coding questions that get great answers
---
+++ oopAndTdd/intro/introOopAndTdd.js
@@ -0,0 +1,51 @@
+//! Intro to OOP (Object-Oriented Programming) and TDD (Test-Driven Development)
+
+// We have learned the javaScript fundamentals.
+
+// Now it's time to level up our skills and begin implementing more powerful processes.
+
+//^ Our goals for this module are:
+
+    // 1. Write clean, readable, maintainable code
+    // 2. Understand and implement basics of Object-Oriented Programming
+    // 3. Test and validate that our code works now and in the future
+    // 4. Construct efficient, scalable data structures and algorithms
+    // 5. Package and export our code for other deelopers to use
+    // 6. Import code from other developers for use in our projects
+
+//! Object-Oriented Programming and Test-Driven Development
+
+// Object-oriented programming is a programming paradigm that promotes modularity by enabling developers to break down their code into smaller, self-contained units (objects).
+
+// Objects are self-contained units of data and code that represent something in the real world and enable us to organize our code into modular, interchangeable components.
+
+// We will also learn to manage context in JavaScript objects.
+
+//! Keys to Success
+
+// Keys to success here are pair programming, problem solving, and communication
+
+//^ Remember Polya's problem-solving framework:
+
+    // 1. Understand the problem
+    // 2. Come up with a plan
+    // 3. Execute the plan
+    // 4. Revisit and refactor your solution
+
+// If you're stuck on execution, revisit the plan.
+
+// If you can't come up with a plan, make sure that you understand the problem.
+
+// If you don't understand the problem, or you do understand but can't come up with a plan, work with your partner to come up with a great coding question.
+
+//^ Great coding questions are:
+
+    // 1. Specific
+    // 2. Clear and concise
+    // 3. Show you put work into it
+
+// Thinking through the problem and writing out a plan will help you ask good questions.
+
+// Clearn, readable code will help your peers, TAs, and instructors to help you quickly.
+
+// Teamwork and communication will lead to success.
