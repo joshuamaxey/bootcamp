48bad0ea8ea825d8dc2692f9d1ee24bd5217537e
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-06-05 22:31:06 -0500

    added notes on object-oriented programming

--- week7x/tuesday/objectOrientedProgramming/oop.js
+++ week7x/tuesday/objectOrientedProgramming/oop.js
@@ -19,3 +19,59 @@
     // 15. Identify when to apply principles of object-oriented programming
 
 //! Introduction to OOP (Object-Oriented Programming)
+
+// In object-oriented programming, we break down large, complex projects into smaller pieces.
+
+// The smaller pieces are then implemented and tested separately.
+
+// OOP often enables us to make our code more readable and maintainable.
+
+//! What is OOP?
+
+// As stated above on line 23, object-oriented programming is a design pattern within which we break down large, complex projects into smaller pieces.
+
+// We use OOP to abstract the implementation details of our code behind APIs which, in turn, interact with other APIs that correspond to different features.
+
+//^ An OBJECT (noun) is an item with its own PROPERTIES (adjectives) and METHODS (verbs)
+
+//^ An OBJECT's characteristics (adjectives) are called PROPERTIES or ATTRIBUTES.
+
+//^ An OBJECT's actions (verbs) are called METHODS.
+
+//! Breaking Down an Example Problem with OOP
+
+// Consider a pen.
+
+//^ In order to determine the properties of a Pen API, think about what we might ask about pen in order to distinguish it from other pens...
+
+    // What is its body color?
+    // What is its ink color?
+    // What is its ink level?
+    // What style is the tip?
+    // Is it refillable?
+    // What is its composition (metal/plastic)?
+
+//^ Now lets translate these qualiteis into property names:
+
+    // .bodyColor
+    // .inkColor
+    // .inkLevel
+    // .tipStyle
+    // .refillable
+    // .composition
+
+//^ To determien the methods of a Pen API, think about: 1. What can it do? 2. What can be done to it?
+
+    // A pen can write
+    // A refillable pen can be refilled
+
+//^ Lets translate these actions into methods:
+
+    // .write()
+    // .refill()
+
+//! Summary
+
+// OOP (object-oriented programming) is an approach to solving problems wherein we break down large projects into smaller parts and abstract implementation details behind APIs which have their own properties and methods.
+
+// Using OOP to break a problem down into smaller parts makes our code more readable, testable, and maintainable
