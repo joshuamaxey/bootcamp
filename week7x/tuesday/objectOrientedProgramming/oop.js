76831cdc536640aa88f19e8bff044057da85fa3c
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-06-05 22:08:18 -0500

    added tuesday projects and began notes on object-oriented programming

---
+++ week7x/tuesday/objectOrientedProgramming/oop.js
@@ -0,0 +1,21 @@
+//! Object-Oriented Programming Objectives
+
+//^ These objectives capture how we will be evaluated on the assessments with respect to this material. We will learn to do the following:
+
+    // 1.  Plan an object-oriented approach to JavaScript development using principles like encapsulation
+    // 2.  Construct a JavaScript class and instantiate instances of that class
+    // 3.  Compare and contrast references to objects vs. references to primitives
+    // 4.  Create a reference to a class instance
+    // 5.  Create a deep copy of a class instance
+    // 6.  Debug errors caused by confusion of references to class instances vs copies of class instances
+    // 7.  Create, identify, and use class instance methods and variables
+    // 8.  Use the 'this' keyword to access and manipulate class instance methods and variables
+    // 9.  Compare and contrast a function and a method
+    // 10. Create, identify, and use class static methods and variables
+    // 11. Apply class inheritance as appropriate
+    // 12. Implement JavaScript classes that inherit from other classes
+    // 13. Modify behavior of a child class using principles of polymorphism such as overriding
+    // 14. Determine whether it's appropriate to override a parent method vs. write a new method for the child class
+    // 15. Identify when to apply principles of object-oriented programming
+
+//! Introduction to OOP (Object-Oriented Programming)
