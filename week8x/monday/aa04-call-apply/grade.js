595ec32c31ca8087057b83eb980dd84d6bb2a044
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-06-10 18:50:12 -0500

    finished call/apply practice

--- week8x/monday/aa04-call-apply/grade.js
+++ week8x/monday/aa04-call-apply/grade.js
@@ -4,8 +4,13 @@ const grades = new GradeBook([100, 50, 90, 20, 30]);
 const gradesAvg = grades.getAverage;
 
 // You can modify the following lines
-let grades1Average = gradesAvg(grades);
-let grades1WithBonus = gradesAvg(grades, [20]);
+
+let grades1Average = gradesAvg.call(grades); // add the .call method to bind the context of the 'gradesAvg' method to the 'grades' variable as its context
+let grades1WithBonus = gradesAvg.apply(grades, [20]); // use the .apply() method to bind the context of the 'gradesAvg' method to the 'grades' object.
+
+//Additionally, note that we use the .apply() method instead of the .call() method here because .apply() takes a second ARRAY parameter as an argument. If our argument is an array, we use the .apply() method.
+
+//If our argument is a single element, or multiple elements separated by commas and NOT contained within an array, we use the .call() method.
 
 console.log('Grade list');
 console.log(grades1Average);     // should print out: 58
--- week8x/monday/aa04-call-apply/greet.js
+++ week8x/monday/aa04-call-apply/greet.js
@@ -6,5 +6,26 @@ function say(name) {
 helloMessage = { message: 'Hello,' };
 heyThereMessage = { message: 'Hey there,' };
 
+//& Original Code:
+
 say(helloMessage, "John");
 say(heyThereMessage, "Michael");
+
+//& New Code:
+
+const say1 = say.bind(helloMessage, "John");
+const say2 = say.bind(heyThereMessage, "Michael");
+
+//^ First, we apply the .bind() metho to both calls of the 'say()' function: say(helloMessage, "John") --> say.bind(helloMessage, "John")
+
+//^ Then we assign both function calls to variables: const say1 = say.bind(helloMessage, "John");
+
+say1(); // prints "Hello, John!"
+say2(); // prints "Hello there, Michael!"
+
+//^ Finally, we invoke both of the functions by calling the variables we saved them to. But this is actually supposed to be a CALL AND APPLY practice, NOT a bind practice, so let's change this up.
+
+say.call(helloMessage, "John");
+say.call(heyThereMessage, "Michael");
+
+//^ Here, we use the .call() method to bind the context of the .say() function to the correct context and then invoke both functions immediately. Because the .call() method immediately invokes the functions, there is no need to save them to a variable.
