a7960d5fac5a22cabf1d9d0b6aa748c44ca69388
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-06-10 19:08:24 -0500

    began notes on TDD

---
+++ week8x/tuesday/testDrivenDevelopment/objectives.js
@@ -0,0 +1,15 @@
+//! Test-Driven Development
+
+//^ As programmers, we need to be able to do the following. These objectives capture how we will be evaluated on assessments with respect to this material:
+
+    // 1.  Throw, catch, and create errors
+    // 2.  Identify and effectively research common JavaScript error types
+    // 3.  Compare and contrasit unit testing, integration testing, and end-to-end testing
+    // 4.  Create, modify, and write code to pas a suite of Mocha tests using the Chai assertion library
+    // 5.  Understand and apply the Red-Green-Refactor cycle of Test-Driven Development
+    // 6.  Read and understand test specs, refer to and navigate the Mocha and Chai docs when we encounter unfamiliar syntax
+    // 7.  Construct good questions basedo n error messages
+    // 8.  Write unit tests using the SIngle Responsibility Principle and DRY
+    // 9.  Plan for appropriate test coverage (???)
+    // 10. Identify and test for edge cases
+    // 11. Interpret test specs to determine a project's requirements
