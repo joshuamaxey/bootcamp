4082108b029be33982226554d5adcc3e162e817f
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-06-11 20:48:13 -0500

    figured out how to correctly import the chai assertion library using a dynamic import and then finished notes on unit testing with mocha and chai including the testing-demo directory

--- week8x/tuesday/testDrivenDevelopment/testing-demo/test/user-spec.js
+++ week8x/tuesday/testDrivenDevelopment/testing-demo/test/user-spec.js
@@ -1,8 +1,17 @@
 // const { expect } = require('chai')
 
-const { chai } = import('chai');
+// const { expect } = import('chai');
 
 //^ Notice that here, we threw an error because the 'require' keyword is no longer supported for the chai import. The incorrect code is commented out above the correct import, which is on line 3 above.
+
+let expect;
+
+(async () => {
+  const chai = await import('chai');
+  expect = chai.expect;
+})();
+
+//^ Turns out that dynamic import (lines 9 - 12) is the ONLY solution to this problem that doesn't require me to change the version of chai/mocha or switch all of my syntax in all of my notes to ES6.
 
 const User = require('../class/user')
 
@@ -20,6 +29,13 @@ describe ('User class', function() {
 
         expect(user).to.exist;
     });
+
+    it ('should set username on creation', function() {
+
+        let user = new User("john_doe");
+
+        expect(user.username).to.equal("john_doe");
+    })
 })
 
 //^ We should be able to understand what this test is doing even if we don't know any Mocha syntax.
@@ -38,4 +54,37 @@ describe ('User class', function() {
 
 // Now, run 'mocha' in the terminal.
 
-//! This shit doesn't work. Fuck it.
+//! Part Four: Specifying Functionality with TEsts
+
+// According to our project, all users should require a username upon creation. We acn specify this in the test specs using a new test (see line 33):
+
+// So now lets return to the blank user class and implement a constructor that will enable us to pass this test.
+
+// Now that we've implemented the constructor function within our previously blank User class, we're passing both test specs.
+
+//! DRYing Your Tests with Mocha Hooks
+
+//^ When we want to test a class, like our user class, we may need to reate new instances of that class before each test runs. We can clean up this repettion using Mocha Hooks (before or beforeEach)
+
+describe('User class', function() {
+
+    let user;
+
+    this.beforeEach(() => {
+        user = new User("john_doe");
+    });
+
+    it('should create successfully', function() {
+        expect(user).to.exist;
+    });
+
+    it('should set username on creation', function() {
+        expect(user.username).to.equal('john_doe');
+    });
+});
+
+//^ By using hooks, we eliminate the need to create numerous instances of a certain class.
+
+//! Summary
+
+// Learned how to use the MOCHA testing framework and the CHAI assertion library to write unit tests for a class.
