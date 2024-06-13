586a8a06a41a8d9ba12686dd4e30484ed34d0c10
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-06-12 19:15:08 -0500

    finished unit testing practice

--- week8x/wednesday/aa04-unit-testing/funcs.js
+++ week8x/wednesday/aa04-unit-testing/funcs.js
@@ -1,13 +1,39 @@
 function isFive(num) {
-  // Your code here 
+  // Your code here
+  // Should return true if the number is 5 otherwise false
+
+  if (num === 5) return true;
+
+  else return false;
+
 }
 
 function isOdd(number) {
-  // Your code here 
+  // Your code here
+
+  // should throw an error if num is not type of number
+  if (typeof number !== "number") throw new Error //^ Note that THIS condition neds to come first. If the input type is NOT a number and we try the modulo statements first, it could cause unexpected behavior that may result in failing the spec.
+
+  // should return true if number is odd
+  if (number % 2 !== 0) return true;
+
+  // should return false if num is even
+  if (number % 2 === 0) return false;
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here 
+  // if step is not provided, should return an array of all numbers from min to max (inclusive) with a default step of 1
+  // if step is provided, shoudl return an array of all numbers from min to max (inclusive) with the provided step as the increment
+
+  let arr = []; // initialize an empty array to hold the values
+
+  for (let i = min; i <= max; i+= step) { // iterate from the min to the max, incrementing by the step. Since the step is set to 1 by default, we don't need to account for whether the step is provided. If it is not provided, it will default to the value of 1.
+
+    arr.push(i); // Push the element located at the current index of the array to 'arr'
+  }
+
+  return arr; // After the loop is complete, return 'arr'
+
 }
 
 
--- week8x/wednesday/aa04-unit-testing/test/class-spec.js
+++ week8x/wednesday/aa04-unit-testing/test/class-spec.js
@@ -6,29 +6,61 @@ const { Word } = require("../class");
 describe("Word", function () {
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+
+      let word = new Word("test")
+
+      expect(word.word).to.equal("test")
+      // expect.fail("replace with your code");
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+
+      let word = new Word("word")
+
+      expect(word.word).to.equal("word")
+      // expect.fail("replace with your code");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+
+      let word1 = new Word("Apple");
+      let word2 = new Word("Test");
+      let word3 = new Word("Orange");
+
+      expect(word1.removeVowels()).to.equal("ppl");
+      expect(word2.removeVowels()).to.equal("Tst");
+      expect(word3.removeVowels()).to.equal("rng");
+      // expect.fail("replace with your code");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+
+      let word1 = new Word("Running");
+      let word2 = new Word("Triangle");
+      let word3 = new Word("JavaScript");
+
+      expect(word1.removeConsonants()).to.equal("ui");
+      expect(word2.removeConsonants()).to.equal("iae");
+      expect(word3.removeConsonants()).to.equal("aai");
+      // expect.fail("replace with your code");
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+
+      let word1 = new Word("Test");
+      let word2 = new Word("JavaScript");
+      let word3 = new Word("Apple");
+
+      expect(word1.pigLatin()).to.equal("estTay");
+      expect(word2.pigLatin()).to.equal("avaScriptJay");
+      expect(word3.pigLatin()).to.equal("Appleyay");
+      // expect.fail("replace with your code");
     });
   });
 });
