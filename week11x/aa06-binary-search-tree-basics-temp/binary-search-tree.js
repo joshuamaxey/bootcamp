33cda9e3a55d2d747821dff6367e60486e0389c9
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-07-09 14:58:08 -0500

    more work on binary search tree practice

--- week11x/aa06-binary-search-tree-basics-temp/binary-search-tree.js
+++ week11x/aa06-binary-search-tree-basics-temp/binary-search-tree.js
@@ -96,22 +96,70 @@ class BinarySearchTree {
   }
 
   search(val) {
-    // Your code here
+
+    let currentNode = this.root;
+
+    while (currentNode) {
+
+      // if (currentNode.val === val) return true;
+
+      // else if (val < currentNode.val) {
+
+      //   currentNode = currentNode.left;
+      // } else {
+
+      //   currentNode = currentNode.right;
+      // }
+
+      if (currentNode.val === val) return true;
+
+      else if (val < currentNode.val) {
+        currentNode = currentNode.left;
+      }
+
+      else if (val > currentNode.val) {
+        currentNode = currentNode.right;
+      }
+    }
+
+    return false;
   }
 
 
   preOrderTraversal(currentNode = this.root) {
-    // Your code here
+
+    if (currentNode) {
+
+      console.log(currentNode.val);
+
+      this.preOrderTraversal(currentNode.left);
+      this.preOrderTraversal(currentNode.right);
+    }
   }
 
 
   inOrderTraversal(currentNode = this.root) {
-    // Your code here
+
+    if (currentNode) {
+
+      this.inOrderTraversal(currentNode.left);
+
+      console.log(currentNode.val);
+
+      this.inOrderTraversal(currentNode.right);
+    }
   }
 
 
   postOrderTraversal(currentNode = this.root) {
-    // Your code here
+
+    if (currentNode) {
+
+      this.postOrderTraversal(currentNode.left);
+      this.postOrderTraversal(currentNode.right);
+
+      console.log(currentNode.val);
+    }
   }
 
     // Breadth First Traversal - Iterative
