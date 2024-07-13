aaac1801dd0849d36faeb033ea218484c609fbd2
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-07-12 20:04:03 -0500

    finished merge-sort practice

--- week11x/sorting/practices/aa06-mergeSort/merge-sort.js
+++ week11x/sorting/practices/aa06-mergeSort/merge-sort.js
@@ -3,15 +3,21 @@
 function mergeSort(arr) {
 
   // Check if the input is length 1 or less
+  if (arr.length <= 1) return arr;
     // If so, it's already sorted: return
 
   // Divide the array in half
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
 
   // Recursively sort the left half
+  left = mergeSort(left);
   // Recursively sort the right half
+  right = mergeSort(right);
 
   // Merge the halves together and return
-
+  return merge(left, right);
 }
 
 
@@ -19,11 +25,32 @@ function mergeSort(arr) {
 function merge(arrA, arrB) {
 
   // Create an empty return array
+  let res = [];
 
   // Point to the first value of each array
+  let arrAIndex = 0;
+  let arrBIndex = 0;
+
   // While there are still values in each array...
+  while (arrAIndex < arrA.length && arrBIndex < arrB.length) {
     // Compare the first values of each array
     // Add the smaller value to the return array
+    if (arrA[arrAIndex] < arrB[arrBIndex]) {
+
+      res.push(arrA[arrAIndex]);
+      arrAIndex++
+    } else {
+      res.push(arrB[arrBIndex]);
+      arrBIndex++
+    }
+  }
+  if (arrAIndex < arrA.length) {
+    res = res.concat(arrA.slice(arrAIndex));
+  } else {
+    res = res.concat(arrB.slice(arrBIndex));
+  }
+
+  return res;
     // Move the pointer to the next value in that array
 
   // Return the return array
@@ -31,4 +58,3 @@ function merge(arrA, arrB) {
 }
 
 module.exports = [merge, mergeSort];
-
