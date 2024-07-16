2e9a71b8dac5b3e6e4c2a12b03668df4f1fc5ef4
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-07-16 13:24:42 -0500

    finished breadth-first-traversal-refactor practice

--- week12x/graphs/practices/aa06-breadth-first-traversal-refactor/problems/01-breadth-first-traversal-refactor.js
+++ week12x/graphs/practices/aa06-breadth-first-traversal-refactor/problems/01-breadth-first-traversal-refactor.js
@@ -17,7 +17,29 @@ const adjList = {
 }
 
 function breadthFirstTraversal(start) {
-  // Your code here 
+
+  let queue = []; // initialize a queue to perform our breadth-first search
+  let visited = new Set(); // initialize a new set to track which nodes we have visited
+  let arr = []; // initialize a new array to hold the nodes that we visit throughout our traversal
+
+  queue.push(start); // enqueue the start node
+  visited.add(start); // add the start node to our 'visited' set
+
+  while (queue.length > 0) { // While the queue is not empty...
+
+    let currentNode = queue.shift(); // shift the current node off the front of the queue and save it in a variable 'currentNode
+    arr.push(currentNode); // push the currentNode to our new array
+
+    for (let neighbor of adjList[currentNode]) { // For each neighbor of our currentNode...
+
+      if (!visited.has(neighbor)) { // ...IF the neighbor is not already present in our 'visited' set...
+
+        queue.push(neighbor); // ...push the neighbor to the queue, and...
+        visited.add(neighbor); // ...add the neighbor to our visited set
+      }
+    }
+  }
+  return arr; // After we have traversed the entire graph, return the array
 }
 
 // console.log(breadthFirstTraversal(3)); // [3, 2, 4, 1, 5, 6]
