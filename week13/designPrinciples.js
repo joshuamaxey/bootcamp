0cb3e325b71645f2c69a7590228d6fde10930aa9
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-07-22 15:38:01 -0500

    began notes on web design principles

---
+++ week13/designPrinciples.js
@@ -0,0 +1,62 @@
+//! Dieter Rams' 10 Principles for Good Design:
+
+//^ 1. Innovative (featuring new methods; advanced and original)
+
+//^ 2. Useful
+
+//^ 3. Aesthetic (concerned with beauty or the appreciation of beauty)
+
+//^ 4. Understandable
+
+//^ 5. Unobtrusive (not conspicuous or attracting attention)
+
+//^ 6. Honest
+
+//^ 7. Long-lasting
+
+//^ 8. Thorough down to the last detail
+
+//^ 9. Environmentally friendly
+
+//^ 10. Minimal (characterized by the use of simple or primary forms or structures)
+
+//! Skeumorphic vs. Flat Design
+
+//^ Skeumorphic Design is when digital components resemble physical components (depth, gradients, etc to make digital buttons, etc appear like real, physical buttons) (pre-2013)
+
+//^ Flat Design exchanges the gradients and shadows of skeumorphic design for flat colors and thin, solid outlines. (2013 - now)
+
+// The change from skeumorphic to flat design in tech over the course of the last decade is called a SUBTRACTIVE APPROACH.
+
+//! Modern Web Design Principles
+
+//^ White Space
+
+// (ctrl + click)
+
+//* https://www.seguetech.com/whitespace-web-design/
+
+// White space is the space we leave around text, images, and interactive elements in our design. White space can be used to improve the legibility and usability of our designs.
+
+// On the other hand, too little white space results in buttons that are too close, text that is difficult to read, and messy, cluttered interfaces.
+
+// "As a designer, I’ve always believed that whitespace is THE fundamental building block of good design. When designers talk about whitespace, they actually mean negative space, the space between elements in a composition."
+
+//^ Advantages of Using Whitespace:
+
+    // 1. Increased Content Legibility
+    // 2. More Interaction
+    // 3. Ability to highlight 'call to actions'
+    // 4. A tidy site = an impressive site
+    // 5. Creates balance
+    // 6. Acts as a separator
+
+// See Also: Design principles 3, 4, 5, and 10 above
+
+//^ Colors
+
+//* https://m2.material.io/design/color/the-color-system.html
+
+
+
+//^ Typography
