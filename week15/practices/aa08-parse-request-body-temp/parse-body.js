af9fc9f62452db9cd8d2784ffb08419f874afd17
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-08-09 11:12:23 -0500

    finished parse-body practice

--- week15/practices/aa08-parse-request-body-temp/parse-body.js
+++ week15/practices/aa08-parse-request-body-temp/parse-body.js
@@ -1,25 +1,51 @@
 function firstStep(input) {
-  // Your code here 
+
+  return input.split('&'); // First, separate key-value pairs within the input string by separating at the "&" ("username=azure+green&password=password%21" = ['username=azure+green', 'password=password%21'])
 }
 
 function secondStep(input) {
-  // Your code here 
+
+  // console.log(input.map((kvp) => kvp.split("=")));
+  return input.map((kvp) => kvp.split("=")); // Second, use the .map() method to .split() to separate the key from the value at the "=" for each key-value pair ("username=azure+green" = "'username', 'azure+green'")
 }
 
 function thirdStep(input) {
-  // Your code here 
+
+  // return input.map(([k, v]) => [k,v.replace(/\+/g," ")]); // Third, target any "+" symbols located in the 'value' of each key-value pair and replace them with spaces ("azure+green" = "azure green")
+
+  for (let i = 0; i < input.length; i++) {
+
+    for (let j = 0; j < input[i].length; j++) {
+
+      input[i][j] = input[i][j].replace("+", " ");
+
+    }
+  }
+  return input;
 }
 
 function fourthStep(input) {
-  // Your code here 
+
+  return input.map(([k, v]) => [k, decodeURIComponent(v)]); // Fourth, decode any parts of the value that has been encoded using Percent Encoding ("password%21" = "password!")
 }
 
 function fifthStep(input) {
-  // Your code here 
+
+  return input.reduce((acc, [k, v]) => {
+    acc[k] = v;
+    return acc;
+  }, {}); // Fifth, turn the key-value pairs into an object
 }
 
 function parseBody(str) {
-  // Your code here 
+
+  first = firstStep(str);
+  second = secondStep(first);
+  third = thirdStep(second);
+  fourth = fourthStep(third);
+  fifth = fifthStep(fourth);
+
+  return fifth;
 }
 
 /******************************************************************************/
--- week15/practices/aa08-parse-request-body-temp/server.js
+++ week15/practices/aa08-parse-request-body-temp/server.js
@@ -5,7 +5,35 @@ let server;
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
-// Your code here 
+const http = require('http'); // import server module
+
+server = http.createServer((req, res) => { // create the server using .createServer method with req & res as arguments
+
+    // console.log("Successfully started the server on port 5000")
+
+    console.log(req.method, req.url);
+
+    let reqBody = '';
+
+    req.on('data', (data) => {
+
+        reqBody += data;
+    });
+
+    req.on('end', () => {
+
+        req.body = parseBody(reqBody);
+        // console.log(reqBody);
+
+        sendFormPage(req, res);
+    });
+
+    // parseBody(reqBody);
+})
+
+const port = 5000;
+
+server.listen(port, () => console.log("Server is listening on port", port)); // tell the server to listen to requests through our chosen port.
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
