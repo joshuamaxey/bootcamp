6eaca2cdaa5ee611feee37316d1cd936bb703c87
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-11-02 12:54:53 -0500

    finished react router navigation practice

--- mod5/week27/practices/aa14-react-router-navigation/src/App.jsx
+++ mod5/week27/practices/aa14-react-router-navigation/src/App.jsx
@@ -1,9 +1,7 @@
-import { createBrowserRouter, RouterProvider, Outlet, NavLink } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, NavLink, Outlet } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
-
-
 
 const router = createBrowserRouter([
   {
@@ -24,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: '*',
         element: <h1>Page Not Found</h1>
+      },
+      {
+        path: '/not-logged-in',
+        element: <h1>You Must Be Logged In To Enter.</h1>
       }
     ]
   }
@@ -37,24 +39,20 @@ function App() {
 
 function Layout() {
   return (
-    <div className='app'>
+    <div className="app">
       <h1>App Component</h1>
-      <nav className='comp nav'>
-        <main>
-          <Outlet />
-        </main>
+      <nav className="comp nav">
         <ul>
-          <NavLink to='/' className={({isActive}) => isActive? 'purple' : ''}>Home</NavLink>
-          <NavLink to='/stocks' className={({isActive}) => isActive? 'purple' : ''}>Stocks</NavLink>
-          <a href='/movies'>Movies-Anchor</a>
+          <li><NavLink to='/' className={({isActive}) => isActive? 'purple' : ''}>Home</NavLink></li>
+          <li><NavLink to='/stocks' className={({isActive}) => isActive? 'purple' : ''}>Stocks</NavLink></li>
+          <li><a href="/movies" >Movies</a></li>
         </ul>
       </nav>
+      <main>
+        <Outlet />
+      </main>
     </div>
-  ),
-  {
-    path: '/not-logged-in',
-    element: <h1>You Must Be Logged In To Enter.</h1>
-  }
+  );
 }
 
 export default App;
--- mod5/week27/practices/aa14-react-router-navigation/src/components/Stocks/Stocks.jsx
+++ mod5/week27/practices/aa14-react-router-navigation/src/components/Stocks/Stocks.jsx
@@ -1,7 +1,23 @@
+import { Navigate, useNavigate } from 'react-router-dom';
+
+let loggedIn = true;
+
 function Stocks() {
+
+  const navigate = useNavigate()
+  if (!loggedIn) {
+    return <Navigate to="/not-logged-in" replace={ true } />
+  }
+
+  const handleClick = () => {
+    window.alert('Sending info to the DB!');
+    navigate('/');
+  };
+
   return (
     <div className='comp orange'>
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
