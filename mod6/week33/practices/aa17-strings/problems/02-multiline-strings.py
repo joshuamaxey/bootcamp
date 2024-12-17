c6cd08e2413e93be34bb83f70346a2068b0d5568
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-12-17 16:10:15 -0600

    Finished Strings Project

---
+++ mod6/week33/practices/aa17-strings/problems/02-multiline-strings.py
@@ -0,0 +1,22 @@
+# It's time to write your first multiline of code in Python. Use the `print` to
+# send a multiline message with a quote in it to the terminal.
+
+# Here is an example of using print to display message to the user:
+
+print('''In this code include "quotes"
+of any kind because they won't get confused with
+the end of the string!''')
+
+# Display your own message using print below:
+
+print('''This
+      is
+      a
+      multi-line
+      string.
+      I didn't know
+      that we can use
+      either 'type'
+      of "quotes" here,
+      that's
+      cool''')
