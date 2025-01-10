482e1eeeae44bf69dd8ef461889494525695c0da
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-01-10 12:11:34 -0600

    Began Calendar Me project

--- mod6/week35/learning-sqlalchemy/pony_owners.py
+++ mod6/week35/learning-sqlalchemy/pony_owners.py
@@ -1,1 +1,21 @@
-from sqlalchemy import create_engine
+"""
+Create an engine with this url:
+sqlite:///dev.db
+"""
+
+from sqlalchemy import create_engine, text
+
+engine = create_engine("sqlite:///dev.db")
+
+with engine.connect() as connection:
+    result = connection.execute(
+        text("""
+        SELECT o.first_name, o.last_name, p.name
+        FROM owners o
+        JOIN ponies p ON (o.id = p.owner_id)
+        """)
+    )
+    for row in result:
+        print(row[0], row[1], "owns", row[2])
+
+engine.dispose()
